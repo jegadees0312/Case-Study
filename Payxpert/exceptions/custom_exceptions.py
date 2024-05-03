a39160ffc837f65b74107a2a6cd30d68b32443f4
Jegadeeswaran <132770892+jegadees0312@users.noreply.github.com>
2024-05-03 18:36:02 +0800

    Add files via upload

---
+++ Payxpert/exceptions/custom_exceptions.py
@@ -0,0 +1,18 @@
+
+class EmployeeNotFoundException(Exception):
+    pass
+
+class PayrollGenerationException(Exception):
+    pass
+
+class TaxCalculationException(Exception):
+    pass
+
+class FinancialRecordException(Exception):
+    pass
+
+class InvalidInputException(Exception):
+    pass
+
+class DatabaseConnectionException(Exception):
+    pass
